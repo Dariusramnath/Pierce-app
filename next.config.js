fff4befb82f6efdefabecb726c4e56fc6e445d00
Dariusramnath <dariusramnath6@gmail.com>
2024-01-19 01:10:03 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ next.config.js
@@ -0,0 +1,45 @@
+// @ts-check
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  typescript: {
+    ignoreBuildErrors: process.env.NEXT_PUBLIC_IGNORE_BUILD_ERROR === "true",
+  },
+  eslint: {
+    ignoreDuringBuilds: process.env.NEXT_PUBLIC_IGNORE_BUILD_ERROR === "true",
+  },
+  webpack: config => {
+    config.resolve.fallback = { fs: false, net: false, tls: false };
+    return config;
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/upload",
+        destination: "https://docjanff.onrender.com/api/upload_video/scaffold_vid.mp4/",
+      },
+      {
+        source: "/generate",
+        destination: "https://49fd5f7c-0d25-427c-9860-bca614076d07-00-1fr3xisq48t8v.riker.replit.dev/api_call",
+        // destination: "https://docjan.varounhanooman4.repl.co/api_call/",
+      },
+      {
+        source: "/generate_specific",
+        destination: "https://49fd5f7c-0d25-427c-9860-bca614076d07-00-1fr3xisq48t8v.riker.replit.dev/api_generate",
+        // destination: "https://docjan.varounhanooman4.repl.co/api_call/",
+      },
+      {
+        source: "/response",
+        destination: "https://49fd5f7c-0d25-427c-9860-bca614076d07-00-1fr3xisq48t8v.riker.replit.dev/response",
+        // destination: "https://docjan.varounhanooman4.repl.co/api_call/",
+      },
+      {
+        source: "/get_files/:path*",
+        destination: "https://49fd5f7c-0d25-427c-9860-bca614076d07-00-1fr3xisq48t8v.riker.replit.dev/get_files/:path*",
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
